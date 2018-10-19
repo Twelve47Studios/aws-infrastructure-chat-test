9d913887ddcb53743cef391293655ff2465c34cc
Nicholas Schiavi <nicholas.schiavi@visaops.com>
2018-10-19 10:40:48 -0700

    test2

--- route53_backup_v1.py
+++ route53_backup_v1.py
@@ -42,6 +42,6 @@ def push_to_github(commit_message):
   repo_dir = os.getcwd()
   repo = Repo(repo_dir)
   repo.git.add(u=True)
-  repo.git.commit(commit_message)
+  repo.git.commit(m=commit_message)
   origin = repo.remote('origin')
   origin.push()
